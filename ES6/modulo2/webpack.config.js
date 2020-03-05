3930309f795c6b401544f2a71d9aae9ade2134e0
Rafael Mardegan <Rafa@OPTIMUS>
2020-03-05 12:31:40 -0300

    Studying 2nd module

---
+++ ES6/modulo2/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
